3429814ca321fccacc9bc035c8208e378f6d415b
bbter <1035675608@qq.com>
2020-10-14 12:23:06 +0800

    deploy

---
+++ precache-manifest.e407708b924e82393533cc56e6d3c79c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0330b231c6b39a3c2c22",
+    "url": "/morney-website/css/app.3a5abaaf.css"
+  },
+  {
+    "revision": "bf16c47319aafabc3606c524a0ac41db",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "0330b231c6b39a3c2c22",
+    "url": "/morney-website/js/app.4a93c57a.js"
+  },
+  {
+    "revision": "173cf108158b30d7a580",
+    "url": "/morney-website/js/chunk-vendors.65ce453d.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
